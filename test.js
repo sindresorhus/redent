12fd88e36e3eed7a3b9cf88c8a8d2b92e984ca93
Sindre Sorhus <sindresorhus@gmail.com>
2015-09-29 17:46:55 +0700

    init

---
+++ test.js
@@ -0,0 +1,8 @@
+import test from 'ava';
+import fn from './';
+
+test(t => {
+	t.is(fn('\n  foo\n    bar\n', 1), '\n foo\n   bar\n');
+	t.is(fn('\n foo\n  bar\n', 5, '-'), '\n-----foo\n----- bar\n');
+	t.end();
+});
