d057e012de69af434456bbf0696ec053e1306d55
Sindre Sorhus <sindresorhus@gmail.com>
2016-07-11 00:04:02 +0200

    [BREAKING] ES2015ify - Requires Node.js >=4 now

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 import test from 'ava';
-import fn from './';
+import m from './';
 
 test(t => {
-	t.is(fn('\n  foo\n    bar\n', 1), '\n foo\n   bar\n');
-	t.is(fn('\n foo\n  bar\n', 5, '-'), '\n-----foo\n----- bar\n');
+	t.is(m('\n  foo\n    bar\n', 1), '\n foo\n   bar\n');
+	t.is(m('\n foo\n  bar\n', 5, '-'), '\n-----foo\n----- bar\n');
 });
